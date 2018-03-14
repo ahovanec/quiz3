d3faf9dcda1b55b43c704ddd82628491c91f3e92
ahovanec <36490538+ahovanec@users.noreply.github.com>
2018-03-13 21:32:36 -0400

    Add files via upload

---
+++ quiz3/FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -0,0 +1,52 @@
+package pkgCore;
+
+import java.util.ArrayList;
+
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+
+public class Deck {
+
+
+	
+	ArrayList<Card> deck = new ArrayList<Card>();
+	
+	public Deck(int numDecks) {
+		
+		for (int i = 0; i < numDecks; i++) {
+			
+			for (int j = 0; j < 4; j++) {
+				
+				for (int k = 0; k < 13; k++) {
+					
+					deck.add(new Card(eRank.values()[k], eSuit.values()[j]));
+
+				}
+				
+			}
+			
+		}
+		
+		
+	}
+
+	
+	  public int getRemaining(Object eNum) {
+	        int remainder = 0;
+	        if(eNum instanceof eSuit) {
+	            for (Card c : this.deck) {
+	                if (c.getSuit() == eNum) {
+	                    remainder += 1;
+	                }
+	            }
+	        }
+	        else {
+	            for (Card c : this.deck) {
+	                if (c.getRank() == eNum) {
+	                    remainder += 1;
+	                }
+	            }
+	        }  
+	        return remainder;
+	    }
+}
